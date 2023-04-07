79a511743a441f219d7d986fe340ee64ca9e0f36
Sophia Okosodo <sophieokosodo@gmail.com>
2023-04-07 11:59:34 +0100

    navbar done

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const productCard = document.getElementById('productModal')
+const techCard = document.getElementById('techModal')
+
+const loadPage = () => {
+    productCard.style.display = 'none'
+    techCard.style.display = 'none'
+
+}
+
+const ShowProductModal = () => {
+    if(techCard.style.display === 'flex'){
+        techCard.style.display = 'none'
+    }
+    productCard.style.display = 'flex'
+}
+const ShowTechModal = () => {
+    if(productCard.style.display === 'flex'){
+        productCard.style.display = 'none'
+    }
+    techCard.style.display = 'flex'
+}
+const hideProductModal = () => {
+    productCard.style.display = 'none'
+}
+const hideTechModal = () => {
+    techCard.style.display = 'none'
+}
